56481b30c09a2693891c3890aba8f1670bbdb81b
maiconbahls <maiconbahls@gmail.com>
2025-10-27 16:55:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -59,4 +59,42 @@ def buscar_status(df, nome_digitado):
     return candidato_encontrado_info
 
 # --- INTERFACE GRÁFICA (FRONT-END) ---
-st.set_page_config(page
+st.set_page_config(page_title="Status do Processo", layout="centered")
+st.title("Consulta de Status do Candidato")
+
+df_candidatos = carregar_dados_google()
+
+if df_candidatos is not None:
+    nome_candidato = st.text_input("Digite o nome do Candidato:")
+    
+    if st.button("Buscar"):
+        if not nome_candidato:
+            st.warning("Por favor, digite o nome de um candidato.")
+        else:
+            resultado = buscar_status(df_candidatos, nome_candidato)
+            
+            if resultado:
+                fase_atual = resultado['fase']
+                nome_oficial = resultado['nome']
+                index_atual = FASES_LIST.index(fase_atual)
+                st.header(f"Status para: {nome_oficial}")
+
+                for i, nome_fase in enumerate(FASES_LIST):
+                    if i <= index_atual:
+                        st.success(f"✔️ {nome_fase}")
+                    else:
+                        st.info(f"⚪ {nome_fase}")
+
+                if fase_atual == FASES_LIST[-1]:
+                    st.balloons() 
+                    st.markdown(
+                        """
+                        <div style="padding: 15px; border: 2px solid #00008B; border-radius: 10px; background-color: #F0F8FF;">
+                        <h2 style="color: #00008B; text-align: center;">PARABÉNS!</h2>
+                        <h3 style="color: #00008B; text-align: center;">VOCÊ É O NOVO BOLSISTA DO CICLO 2026</h3>
+                        </div>
+                        """, 
+                        unsafe_allow_html=True
+                    )
+            else:
+                st.error("O candidato não foi encontrado na base de dados.")
